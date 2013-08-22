65bea20d6161166bb4353ceaf10881f7fb30e670
Jon Laing <jlaing@brain-scape.com>
2013-08-22 17:47:53 -0400

    added some documentation to the code

--- sql_utils.go
+++ sql_utils.go
@@ -1,9 +1,11 @@
+// A group of utitlities to deal with SQL in a more Object Oriented way.
 package sql_utils
 
 import (
   "reflect"
   "strings"
   "regexp"
+  "database/sql"
 )
 
 type SqlUtil struct {
@@ -11,6 +13,27 @@ type SqlUtil struct {
 }
 
 
+/*
+Create a MySql table based on the fields of a struct.
+
+Example:
+  type Model struct {
+    Id    int     `mysql:"id INT NOT NULL AUTO_INCREMENT,pk"`
+    Name  string  `mysql:"name VARCHAR(20) NOT NULL"`
+  }
+
+  var s SqlUtil
+  s.Conn.GetConfiguration("config.gcfg","test")
+
+  err := s.CreateTable("model", &Model{})
+  if err != nil {
+    panic(err)
+  }
+
+The `mysql` tags in the struct are necessary. The portion before the comma will be directly translated to the table. To denote a field as a primary key, simply split the statement with a comma and add `pk` at the end, as shown in the `Id` field above.
+
+This function will only create the table if it doesn not already exist.
+*/
 func (s SqlUtil) CreateTable(tablename string, i interface{}) error {
   db := s.Conn.Open() 
   defer db.Close()
@@ -21,6 +44,8 @@ func (s SqlUtil) CreateTable(tablename string, i interface{}) error {
   return err
 }
 
+// Drop a MySql table. This function will only drop the table if it
+// does not already exist
 func (s SqlUtil) DropTable(tablename string) error {
   db := s.Conn.Open()
   defer db.Close()
@@ -42,6 +67,39 @@ func (s SqlUtil) TableExists(tablename string) bool {
   return true
 }
 
+// Simply a wrapper for Go Lang's `sql.Query`.
+func (s SqlUtil) Query(query string, args ...interface{}) (*sql.Rows, error) {
+  db := s.Conn.Open()
+  defer db.Close()
+  rows, err := db.Query(query, args)
+
+  return rows, err
+}
+
+// Simply a wrapper for Go Lang's `sql.QueryRow`.
+func (s SqlUtil) QueryRow(query string, args ...interface{}) *sql.Row {
+  db := s.Conn.Open()
+  defer db.Close()
+  row := db.QueryRow(query, args)
+
+  return row
+}
+
+/*
+A function used for parsing out the MySQL fields of a struct.
+
+Example:
+  type Model struct {
+    Id    int     `mysql:"id INT NOT NULL AUTO_INCREMENT,pk"`
+    Name  string  `mysql:"name VARCHAR(20) NOT NULL"`
+  }
+
+  var s SqlUtil
+  list := s.FieldList(&Model{})
+
+  // list[0]  = "id INT NOT NULL AUTO_INCREMENT"
+  // list[1]  = "name VARCAHR(20) NOT NULL"
+*/
 func (s SqlUtil) FieldList(i interface{}) (list []string) {
   fields, _ := s.parseFields(i)
 
@@ -50,6 +108,12 @@ func (s SqlUtil) FieldList(i interface{}) (list []string) {
   }
 
   return
+}
+
+func (s SqlUtil) PrimaryKeys(i interface{}) []string {
+  _, pks := s.parseFields(i)
+
+  return pks
 }
 
 func (s SqlUtil) buildCreateTableStatement(tablename string, i interface{}) (create_statement string) {
